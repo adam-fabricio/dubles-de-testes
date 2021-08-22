86a82725f9dbef8305e04b3709f39606779d4e28
Adam <adam.fabricio@gmail.com>
2021-08-22 17:42:29 -0300

    Feature: Duble de testes Modulo 4 Video 4

---
+++ colecao/livros.py
@@ -0,0 +1,165 @@
+from urllib.request import urlopen, Request
+from urllib.parse import urlencode
+from urllib.error import HTTPError
+from math import ceil
+import logging
+import os
+
+
+def consultar_livros(autor):
+    if not isinstance(autor, str):
+        raise Exception("autor tem que ser uma string")
+    dados_para_requisicao = preparar_dados_para_requisicao(autor)
+    url = obter_url("https://buscador", dados_para_requisicao)
+    return executar_requisicao(url)
+
+
+def preparar_dados_para_requisicao(autor):
+    dados = {"autor": autor}
+    return dados
+
+
+def obter_url(url, dados):
+    pass
+
+
+def executar_requisicao(url):
+    try:
+        with urlopen(url, timeout=10) as resposta:
+            resultado = resposta.read().decode()
+    except HTTPError as e:
+        logging.exception(f"Ao acessar {url}: {e}")
+    else:
+        return resultado
+
+
+def escrever_em_arquivo(arquivo, conteudo):
+    diretorio = os.path.dirname(arquivo)
+    try:
+        os.makedirs(diretorio)
+    except OSError as e:
+        logging.exception(f"Nao foi possivel criar o diretorio {diretorio}.")
+    try:
+        with open(arquivo, "w") as fp:
+            fp.write(conteudo)
+    except OSError as e:
+        logging.exception(f"Nao foi possivel criar arquivo {arquivo}.")
+
+
+class Consulta:
+    """
+    Armazena os dados da expressao de busca:
+        - autor, titulo, livre
+        - pagina
+        - url
+        - dados_para_requisicao
+    """
+
+    def __init__(self, autor: str, titulo: str, livre: str) -> None:
+        self._autor: str = autor
+        self._titulo: str = titulo
+        self._livre: str = livre
+        self._pagina: int = 0
+        self._dados_para_requisicao: dict = None
+        self._url = "https://buscarlivros"
+
+
+    @property
+    def pagina(self)->str:
+        return self._pagina
+
+    @property
+    def dados_para_requisicao(self)->str:
+        """Retorna um dicionario com os dados de Consulta."""
+        if not self._dados_para_requisicao:
+            self._dados_para_requisicao = {}
+            if self._livre:
+                self._dados_para_requisicao = {"q": self._livre}
+            else:
+                if self._autor:
+                    self._dados_para_requisicao["autor"] = self._autor
+                if self._titulo:
+                    self._dados_para_requisicao["title"] = self._titulo
+        return self._dados_para_requisicao
+
+
+    @property
+    def seguinte(self):
+        dados_para_requisicao = self.dados_para_requisicao
+        self._pagina += 1
+        dados_para_requisicao["page"] = self._pagina
+        req = Request(self._url, dados_para_requisicao)
+        if req.data:
+            return req.full_url + "?" + urlencode(req.data)
+
+
+class Resposta:
+    """Conteudo da pagina em formato JSON."""
+
+    quantidade_documentos_por_pagina = 50
+
+    def __init__(self, conteudo: str):
+        # Conteudo maximo da pagina pura
+        self._conteudo = conteudo
+        # Conteudo processado, formato dicionario
+        self._dados = None
+
+    @property
+    def conteudo(self):
+        return self._conteudo
+
+    @property
+    def dados(self):
+        if not self._dados:
+            try:
+                j = json.loads(self.conteudo)
+            except TypeError as e:
+                logging.exception(
+                    "Resultado da consulta {self.conteudo}: tipo invalido. "
+                                 )
+            except json.JSONDecodeError as e:
+                logging.exception(
+                    "Resultado da consulta {self.conteudo}: JSON invalido"
+                )
+            else:
+                self._dados = j
+        return self._dados
+
+
+    @property
+    def documentos(self):
+        """Documentos retornados na pagina."""
+        return self.dados.get("docs", [])
+
+
+    @property
+    def total_de_paginas(self):
+        """Total de paginas, todos os resultados."""
+        if len(self.documentos):
+            return   ceil(
+                self.dados.get("num_docs", 0)
+                / self.quantidade_documentos_por_pagina
+                )
+        return 0
+
+
+def baixar_livros(autor, titulo, livre):
+    Consulta(autor, titulo, livre)
+
+"""
+def baixar_livros(self, arquivos, autor=None, titulo=None, livre=None):
+    consulta = Consulta(autor, titulo, livre)
+    total_de_paginas = 1
+    i = 0
+    while True:
+        resultado = executar_requisicao(consulta.seguinte)
+        if resultado:
+            resposta = Resposta(resultado)
+            total_de_paginas = resposta.total_de_paginas
+            escrever_em_arquivo(arquivo[i], resultado)
+        elif consulta.pagina == 1:
+            total_de_paginas = 2
+        if consulta.pagina == total_de_paginas:
+            break
+        i += 1
+"""
---
+++ tests/test_livros.py
@@ -0,0 +1,479 @@
+import pytest
+from unittest import skip
+from unittest.mock import patch, mock_open, MagicMock
+from colecao.livros import (consultar_livros,
+                            executar_requisicao,
+                            escrever_em_arquivo,
+                            Consulta,
+                            baixar_livros
+                            )
+from urllib.request import HTTPError
+
+
+
+class StubHTTPResponse:
+    def read(self):
+        return b''
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, param1, param2, param3):
+        pass
+
+
+@patch("colecao.livros.urlopen", return_value=StubHTTPResponse())
+def test_quando_consultar_livros_deve_retornar_uma_string(stub_urlopen):
+    resultado = consultar_livros("Agatha Christie")
+
+    assert type(resultado) == str
+
+
+@patch("colecao.livros.urlopen", return_value=StubHTTPResponse())
+def test_quando_consultar_livros_deve_chamar_preparar_dados_com_os_mesmos_parametros(stub_urlopen):
+    with patch("colecao.livros.preparar_dados_para_requisicao") as spy_preparar_dados:
+        consultar_livros("Agatha Christie")
+        spy_preparar_dados.assert_called_once_with("Agatha Christie")
+
+
+@patch("colecao.livros.urlopen", return_value=StubHTTPResponse())
+def test_quando_consultar_livros_deve_chamar_obter_url_com_o_retorno_de_preparar_dados_para_requisicao(stub_urlopen):
+    with patch("colecao.livros.preparar_dados_para_requisicao") as stub_preparar_dados:
+        dados = {"author": "Agatha Christie"}
+        stub_preparar_dados.return_value = dados
+        with patch("colecao.livros.obter_url") as spy_obter_url:
+            consultar_livros("Agatha Christie")
+            spy_obter_url.assert_called_once_with("https://buscador", dados)
+
+
+@patch("colecao.livros.urlopen", return_value=StubHTTPResponse())
+def test_quando_consultar_livro_deve_executar_requisicao_usando_retorno_obter_url(stub_urlopen):
+    with patch("colecao.livros.obter_url") as stub_obter_url:
+        stub_obter_url.return_value = "https://buscador"
+        with patch("colecao.livros.executar_requisicao") as spy_executar_requisicao:
+            consultar_livros("Agatha Christie")
+            spy_executar_requisicao.assert_called_once_with("https://buscador")
+
+
+def stub_urlopen(url, timeout):
+    return StubHTTPResponse()
+
+
+def test_quando_executar_requisicao_deve_retornar_string_1():
+    with patch("colecao.livros.urlopen", stub_urlopen):
+        url = "https://buscarlivros?author=J+K+Rowlings"
+        resultado = executar_requisicao(url)
+        assert type(resultado) == str
+
+
+def test_quando_executar_requisicao_deve_retornar_string_2():
+    with patch("colecao.livros.urlopen") as stub_urlopen_2:
+        stub_urlopen_2.return_value = StubHTTPResponse()
+        url = "https://buscarlivros?author=J+K+Rowlings"
+        resultado = executar_requisicao(url)
+        assert type(resultado) == str
+
+
+
+def test_quando_executar_requisicao_deve_retornar_string_3():
+    with patch("colecao.livros.urlopen", return_value=StubHTTPResponse()):
+        url = "https://buscarlivros?author=J+K+Rowlings"
+        resultado = executar_requisicao(url)
+        assert type(resultado) == str
+
+
+@patch("colecao.livros.urlopen", return_value=StubHTTPResponse())
+def test_quando_executar_requisicao_deve_retornar_string_4(stub_urlopen_4):
+    url = "https://buscarlivros?author=J+K+Rowlings"
+    resultado = executar_requisicao(url)
+    assert type(resultado) == str
+
+
+
+@patch("colecao.livros.urlopen")
+def test_quando_executar_requisicao_deve_retornar_string_5(stub_urlopen_5):
+    stub_urlopen_5.return_value = StubHTTPResponse()
+    url = "https://buscarlivros?author=J+K+Rowlings"
+    resultado = executar_requisicao(url)
+    assert type(resultado) == str
+
+
+class Dummy:
+    pass
+
+def stub_urlopen_http_error(url, timeout):
+    fp = mock_open
+    fp.close = Dummy
+    raise HTTPError(Dummy(), Dummy(), "mensagem de erro", Dummy(), fp)
+
+
+@skip("Nao compativel")
+@patch("colecao.livros.urlopen", return_value=StubHTTPResponse())
+def test_quando_executar_requsicao_deve_retornar_http_error_1(stub_urlopen):
+    with patch("colecao.livros.urlopen", stub_urlopen_http_error):
+        with pytest.raises(HTTPError) as excecao:
+            executar_requisicao("https://buscador")
+        assert "mensagem de erro" in str(excecao.value)
+
+@skip("Nao compativel")
+@patch("colecao.livros.urlopen", return_value=StubHTTPResponse())
+@patch("colecao.livros.urlopen")
+def test_quando_executar_requsicao_deve_retornar_http_error_2(stub_urlope, uble_urlopen):
+    fp = mock_open
+    fp.close = Dummy
+    stub_urlopen.side_effect = HTTPError(Dummy(), Dummy(), "mensagem de erro", Dummy(), fp)
+    with pytest.raises(HTTPError) as excecao:
+        executar_requisicao("https://buscador")
+    assert "mensagem de erro" in str(excecao.value)
+
+
+def test_quando_executar_requisicao_deve_logar_http_error(caplog):
+    with patch("colecao.livros.urlopen", stub_urlopen_http_error):
+        resultado = executar_requisicao("https://buscador")
+        mensagem_de_erro = "mensagem de erro"
+        assert len(caplog.records) == 1
+        for registro in caplog.records:
+            assert mensagem_de_erro in registro.message
+
+
+
+@patch("colecao.livros.urlopen")
+def test_quando_executar_requsicao_deve_logar_http_error_2(stub_urlopen, caplog):
+    fp = mock_open
+    mensagem_de_erro = "mensagem de erro"
+    fp.close = Dummy
+    stub_urlopen.side_effect = HTTPError(Dummy(), Dummy(), "mensagem de erro", Dummy(), fp)
+    executar_requisicao("https://buscador")
+    assert len(caplog.records) == 1
+    for registro in caplog.records:
+        assert mensagem_de_erro in registro.message
+
+
+
+class StubLogging():
+    def __init__(self):
+        self._mensagens = []
+
+    @property
+    def mensagens(self):
+        return self._mensagens
+
+    def exception(self, mensagem):
+        self._mensagens.append(mensagem)
+
+
+def stub_makedirs(diretorio):
+    raise OSError(f"Nao foi possivel criar o diretorio {diretorio}")
+
+def teste_quando_escreve_em_arquivo_deve_logar_os_error():
+    arquivo = "/tmp/arquivos.json"
+    conteudo = "dados de livros"
+    stub_logging = StubLogging()
+    with patch("colecao.livros.os.makedirs", stub_makedirs):
+        with patch("colecao.livros.logging", stub_logging):
+            escrever_em_arquivo(arquivo, conteudo)
+            assert "Nao foi possivel criar o diretorio /tmp." in stub_logging.mensagens 
+
+
+
+@patch("colecao.livros.open", side_effect=OSError())
+@patch("colecao.livros.os.makedirs")
+@patch("colecao.livros.logging.exception")
+def test_quando_escreve_em_arquivo_deve_logar_erro_ao_criar_arquivo(spy_exception,
+                                                                    stub_makedirs,
+                                                                    stub_open
+                                                                   ):
+    arq = "/bla/bla/bla.json"
+    escrever_em_arquivo(arq, "dados de livros")
+    spy_exception.assert_called_once_with(f"Nao foi possivel criar arquivo {arq}.")
+
+
+class SpyFp():
+
+    def __init__(self):
+        self._conteudo = None
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, param1, param2, param3):
+        pass
+
+    def write(self, conteudo):
+        self._conteudo = conteudo
+
+    @property
+    def conteudo(self):
+        return self._conteudo
+
+
+@patch("colecao.livros.open")
+def test_quando_escrever_arquivo_deve_chamar_write_1(stub_open):
+    arq = "tmp/arquivo"
+    conteudo = "conteudo do arquivo."
+    spy_de_fp = SpyFp()
+    stub_open.return_value = spy_de_fp
+
+    escrever_em_arquivo(arq, conteudo)
+    assert spy_de_fp.conteudo == conteudo
+
+
+
+@patch("colecao.livros.open")
+def test_quando_escrever_arquivo_deve_chamar_write_2(stub_de_open):
+    arq = "tmp/arquivo"
+    conteudo = "Conteudo do arquivo"
+    spy_de_fp = MagicMock()
+    spy_de_fp.__enter__.return_value = spy_de_fp
+    spy_de_fp.__exit__.return_value = None
+    stub_de_open.return_value = spy_de_fp
+
+    escrever_em_arquivo(arq, conteudo)
+    spy_de_fp.write.assert_called_once_with(conteudo)
+
+
+@pytest.fixture
+def resultado_em_duas_paginas():
+    return [
+        """
+        {
+            "num_docs": 5,
+            "docs": [
+                {"author": "Luciano Ramalho",
+                 "title": "Python Fluent"
+                },
+                {"author": "Nilo Neil",
+                 "title": "Introducao a Programacao com Python"
+                },
+                {"author": "Allen B. Downey",
+                 "title": "Pense em Python"
+                }
+            ]
+        }
+        """,
+        """
+        {
+            "num_docs": 5
+            "docs": [
+                {"author": "Kenneth Reitz",
+                 "title": "O Guia do Mochileiro Python"
+                }.
+                {"author": "Wes McKinney",
+                 "title": "Python Para Abakuse de Dados"
+                },
+            ]
+        }
+        """,
+    ]
+
+
+@pytest.fixture
+def resultado_em_tres_paginas():
+    return [
+        """
+        {
+            "num_docs": 8,
+            "docs": [
+                {"author": "Luciano Ramalho",
+                 "title": "Python Fluent"
+                },
+                {"author": "Nilo Neil",
+                 "title": "Introducao a Programacao com Python"
+                },
+                {"author": "Allen B. Downey",
+                 "title": "Pense em Python"
+                }
+            ]
+        }
+        """,
+        """
+        {
+            "num_docs": 8,
+            "docs": [
+                {"author": "Luciano Ramalho",
+                 "title": "Python Fluent"
+                },
+                {"author": "Nilo Neil",
+                 "title": "Introducao a Programacao com Python"
+                },
+                {"author": "Allen B. Downey",
+                 "title": "Pense em Python"
+                }
+            ]
+        }
+        """,
+        """
+        {
+            "num_docs": 8
+            "docs": [
+                {"author": "Kenneth Reitz",
+                 "title": "O Guia do Mochileiro Python"
+                }.
+                {"author": "Wes McKinney",
+                 "title": "Python Para Abakuse de Dados"
+                },
+            ]
+        }
+        """,
+    ]
+
+
+@pytest.fixture
+def conteudo_de_quatro_arquivos():
+    return [
+        """
+        {
+            "num_docs": 17,
+            "docs": [
+                {"author": "Luciano Ramalho",
+                 "title": "Python Fluent"
+                },
+                {"author": "Nilo Neil",
+                 "title": "Introducao a Programacao com Python"
+                },
+                {"author": "Luciano Ramalho",
+                 "title": "Python Fluent"
+                },
+                {"author": "Nilo Neil",
+                 "title": "Introducao a Programacao com Python"
+                },
+                {"author": "Allen B. Downey",
+                 "title": "Pense em Python"
+                }
+            ]
+        }
+        """,
+        """
+        {
+            "num_docs": 17,
+            "docs": [
+                {"author": "Luciano Ramalho",
+                 "title": "Python Fluent"
+                },
+                {"author": "Nilo Neil",
+                 "title": "Introducao a Programacao com Python"
+                },
+                {"author": "Luciano Ramalho",
+                 "title": "Python Fluent"
+                },
+                {"author": "Nilo Neil",
+                 "title": "Introducao a Programacao com Python"
+                },
+                {"author": "Allen B. Downey",
+                 "title": "Pense em Python"
+                }
+            ]
+        }
+        """,
+        """
+        {
+            "num_docs": 17,
+            "docs": [
+                {"author": "Kenneth Reitz",
+                 "title": "O Guia do Mochileiro Python"
+                }.
+                {"author": "Wes McKinney",
+                 "title": "Python Para Abakuse de Dados"
+                },
+            ]
+        }
+        """,
+    ]
+
+
+@pytest.fixture
+def resultado_em_tres_paginas_erro_na_pagina_2():
+    return [
+        """
+        {
+            "num_docs": 8,
+            "docs": [
+                {"author": "Luciano Ramalho",
+                 "title": "Python Fluent"
+                },
+                {"author": "Nilo Neil",
+                 "title": "Introducao a Programacao com Python"
+                },
+                {"author": "Allen B. Downey",
+                 "title": "Pense em Python"
+                }
+            ]
+        }
+        """,
+        None,
+        """
+        {
+            "num_docs": 8
+            "docs": [
+                {"author": "Kenneth Reitz",
+                 "title": "O Guia do Mochileiro Python"
+                }.
+                {"author": "Wes McKinney",
+                 "title": "Python Para Abakuse de Dados"
+                },
+            ]
+        }
+        """,
+    ]
+
+
+@pytest.fixture
+def resultado_em_tres_pagina_erro_na_pagina_1():
+    return [
+        None,
+        """
+        {
+            "num_docs": 8,
+            "docs": [
+                {"author": "Luciano Ramalho",
+                 "title": "Python Fluent"
+                },
+                {"author": "Nilo Neil",
+                 "title": "Introducao a Programacao com Python"
+                },
+                {"author": "Allen B. Downey",
+                 "title": "Pense em Python"
+                }
+            ]
+        }
+        """,
+        """
+        {
+            "num_docs": 8
+            "docs": [
+                {"author": "Kenneth Reitz",
+                 "title": "O Guia do Mochileiro Python"
+                }.
+                {"author": "Wes McKinney",
+                 "title": "Python Para Abakuse de Dados"
+                },
+            ]
+        }
+        """,
+    ]
+
+
+class MockConsulta:
+    def __init__(self):
+        self.chamadas = []
+        self.consultas = []
+
+
+    def Consulta(self, autor=None, title=None, livre=None):
+        consulta = Consulta(autor, title, livre)
+        self.chamadas.append((autor, title, livre))
+        self.consultas.append(consulta)
+        return consulta
+
+
+    def verifica(self):
+        assert len(self.consultas) == 1
+        assert self.chamadas == [(None, None, "Python")]
+
+
+def test_quando_baixar_livros_instancia_consulta_uma_vez():
+    duble = MockConsulta()
+    with patch("colecao.livros.Consulta", duble.Consulta):
+        baixar_livros(None, None, "Python")
+        duble.verifica()
+
